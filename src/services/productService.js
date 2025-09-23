529953a58e0a91a9f1273648f18e980f4bccee5c
Soumika Devarakonda <142423331+soumikadevarakonda@users.noreply.github.com>
2025-09-23 12:28:10 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -15,5 +15,5 @@ export const getProducts = async (category = "") => {
 
 // Get product image URL
 export const getProductImageUrl = (imagePath) => {
-  return `http://localhost:8080/api/products/images/${imagePath}`;
+  return `http://ec2-51-20-123-105.eu-north-1.compute.amazonaws.com:8080/api/products/images/${imagePath}`;
 };
